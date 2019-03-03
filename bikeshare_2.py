5cbf424fb0164172e44cbe66aa5d0831cfd60b3b
Adithya K Sundaragiri <kalyan.sa@outlook.com>
2019-03-02 23:51:12 -0500

    Changed the Intro Lines

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -23,7 +23,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore Some Key Metrics of the popular BikeShare data in US major Cities')
 
 
     while True:
